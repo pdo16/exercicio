46e37222e7021aee2b829740f2d7f41570e3988b
treinamento <treinamento@indra-PC>
2016-07-27 15:37:22 -0300

    Alteração - Trocar operador de comparação

--- maven/src/main/java/Person.js
+++ maven/src/main/java/Person.js
@@ -14,10 +14,10 @@ Person.prototype = {
 
 var a = NaN;
 
-if (a === NaN) {  // Noncompliant; always false
+if (a !== a) {  // Noncompliant; always false
   console.log("a is not a number");  // this is dead code
 }
-if (a !== NaN) { // Noncompliant; always true
+if (a === a) { // Noncompliant; always true
   console.log("a is not NaN"); // this statement is not necessarily true
 }
 
